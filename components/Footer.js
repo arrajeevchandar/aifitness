a6fe42cd3c1660f12aaf754b2494339c81f1ba8d
rajeev <rajeevarchandar@gmail.com>
2025-03-27 22:57:24 +0530

    added footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,20 @@
 import Link from "next/link";
 
 export default function Footer() {
-	return (
-		<div className={'flex-shrink-0 text-center text-md p-4 border-t bg-white'}>
-			Created by {' '}
-			<Link
-				className={'text-primary-main'}
-				href={"https://www.google.com"}
-				target={'_blank'}
-				rel={'noopener noreferrer'}
-			>
-				A R RAJEEV CHANDAR
-			</Link>
-		</div>
-	)
+  return (
+    <footer className="fixed bottom-0 left-0 w-full z-50 bg-black/60 backdrop-blur-lg shadow-lg border-t border-gray-700 text-center text-lg-start">
+      {/* Copyright Section */}
+      <div className="text-center p-3 text-white">
+        © 2025 Copyright:{" "}
+        <Link
+          className="text-primary font-medium hover:underline"
+          href="/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          AI Fitness Planner
+        </Link>
+      </div>
+    </footer>
+  );
 }
